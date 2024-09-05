85c2a5552f2d9b8c66b09509c3ca3776e8e53854
gianninapaolino@outlook.com <gianninapaolino@outlook.com>
2024-09-05 11:02:20 -0300

    Funcion eliminar y btns

---
+++ js/almacenar.js
@@ -0,0 +1,16 @@
+/* esto es para eliminar un elemento*/
+function eliminarItem(){
+    localStorage.setItem('listaId', JSON.stringify([]));
+    listaContenedor.innerHTML = '';
+  
+}
+
+
+/*le da funcionalidad a los botones*/
+btnAgregar.addEventListener('click', agregarItem);
+btnLimpiar.addEventListener('click', eliminarItem);
+
+
+//Carga la lista al iniciar la página
+cargarLista();
+
