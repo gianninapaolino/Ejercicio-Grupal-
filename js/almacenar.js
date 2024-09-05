7635ffb589987d218667f114939e8c2af53fd27e
Lucia Bacigalupe <luciabacigalupemg@gmail.com>
2024-09-05 10:59:46 -0300

    Constantes

---
+++ js/almacenar.js
@@ -0,0 +1,5 @@
+const listaId = 'lista';
+const inputItem = document.getElementById('item');
+const listaContenedor = document.getElementById('contenedor');
+const btnAgregar = document.getElementById('agregar');
+const btnLimpiar = document.getElementById('limpiar');
