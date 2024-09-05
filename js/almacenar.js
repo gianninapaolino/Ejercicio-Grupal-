55a11389a3b59afaf7aadf780c6b9483d1aafcd0
FaCami <camilafarroyo16842@hotmail.com>
2024-09-05 11:03:39 -0300

    Update almacenar.js
    función para cargar la lista en local storage

--- js/almacenar.js
+++ js/almacenar.js
@@ -3,3 +3,16 @@ const inputItem = document.getElementById('item');
 const listaContenedor = document.getElementById('contenedor');
 const btnAgregar = document.getElementById('agregar');
 const btnLimpiar = document.getElementById('limpiar');
+
+//Cargar lista desde localStorage
+function cargarLista(){
+    const listaGuardada = localStorage.getItem('listaId');
+    if (listaGuardada){
+        const lista = JSON.parse(listaGuardada);
+        lista.forEach(item => {
+            const li = document.createElement('li');
+            li.textContent = item;
+            listaContenedor.appendChild(li);
+        });
+    }
+}
